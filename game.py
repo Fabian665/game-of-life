9014fb417d105c0ac93906e94ac4f3f215b4268a
Fabian665 <fabian665@gmail.com>
2022-06-18 13:06:13 +0300

    Added TextButton class

--- game.py
+++ game.py
@@ -3,11 +3,21 @@ import pygame
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+pygame.init()
+screen_size = (width, height) = (640, 665)
+screen = pygame.display.set_mode(screen_size)
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill(white)
+
+buttons_font = pygame.font.Font(None, 16)
+
 
 class TextButton:
-    button_color = (245, 245, 245)
-    hover_color = (230, 230, 230)
-    click_color = (200, 200, 200)
+    reg_color = (230, 230, 230)
+    hover_color = (220, 220, 220)
+    click_color = (240, 240, 240)
     text_color = (0, 0, 0)
     width = 55
     height = 25
@@ -15,38 +25,38 @@ class TextButton:
     def __init__(self, x, y, text):
         self.x = x
         self.y = y
-        self.text = text
         self.rect = pygame.Rect(x, y, self.width, self.height)
-        self.up_line = ((self.x, self.y), (self.x + self.width, self.y), 1)
-        self.right_line = ((self.x + self.width, self.y), (self.x + self.width, self.y + self.height), 1)
-        self.down_line = ((self.x, self.y + self.height), (self.x + self.width, self.y + self.height), 1)
-        self.left_line = ((self.x, self.y), (self.x, self.y + self.height), 1)
+        self.text_surf = buttons_font.render(text, True, self.text_color)
+        self.text_rect = self.text_surf.get_rect(center=self.rect.center)
+        self.pressed = False
+        self.button_color = (255, 0, 0)
 
-    def draw(self, background):
+    def draw(self):
         global black
-        pygame.draw.rect(background, self.button_color, self.rect)
-        pygame.draw.line(background, black, *self.up_line)
-        pygame.draw.line(background, black, *self.right_line)
-        pygame.draw.line(background, black, *self.down_line)
-        pygame.draw.line(background, black, *self.left_line)
+        self.click_logic()
+        pygame.draw.rect(background, self.button_color, self.rect, border_radius=7)
+        background.blit(self.text_surf, self.text_rect)
+
+    def click_logic(self):
         mouse_pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
-            pygame.draw.rect(background, self.hover_color, self.rect)
+            self.button_color = self.hover_color
+            if pygame.mouse.get_pressed()[0]:
+                self.pressed = True
+                self.button_color = self.click_color
+            elif self.pressed:
+                self.pressed = False
+                self.button_color = self.hover_color
+        else:
+            self.button_color = self.reg_color
 
 
 def main():
     global white
-    pygame.init()
-    screen_size = (width, height) = (640, 665)
-    screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption("Conway's Game of Life")
 
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill(white)
-
-    font = pygame.font.Font(None, 36)
-    text = font.render("Conway's Game of Life - by Fabian", True, (10, 10, 10))
+    font_title = pygame.font.Font(None, 36)
+    text = font_title.render("Conway's Game of Life - by Fabian", True, (10, 10, 10))
     textpos = text.get_rect()
     textpos.centerx = background.get_rect().centerx
     textpos.centery += 5
@@ -55,12 +65,13 @@ def main():
     button = TextButton(30, 30, "Hello")
 
     screen.blit(background, (0, 0))
-    button.draw(background)
+    button.draw()
     pygame.display.flip()
 
     run = True
     while run:
         for event in pygame.event.get():
+            button.draw()
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
@@ -69,7 +80,6 @@ def main():
                 pass
         if run:
             screen.blit(background, (0, 0))
-            button.draw(background)
             pygame.display.flip()
 
 
